3edf0fe168857854d1e975bbccee0f1a12e56f48
Sarath Kumar S <60085197+ceccs18c52@users.noreply.github.com>
2020-03-20 14:04:20 +0530

    t8 (2)

---
+++ 2003/Perl/oddeven.pl
@@ -0,0 +1,14 @@
+print "\n Enter a number " ;
+$num = <STDIN> ;
+if ( $num == 0 )
+{
+   print " Entered number is zero \n" ;
+}
+elsif ( $num % 2 == 0 )
+{
+   print " The value is an Even number \n" ;
+}
+else
+{ 
+   print " The value is an Odd number \n" ;
+}
